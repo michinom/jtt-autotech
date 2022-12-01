83fb4e114e541f3d979f7719aeafa315a016f25b
Rick J <rick.jeavons@gmail.com>
2022-12-01 23:16:34 +0000

    Header and footer to functions

--- src/markup/layout/footer.jsx
+++ src/markup/layout/footer.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 // Images
@@ -12,133 +12,130 @@ const telnumber1 = "658899";
 const telnumber2 = "647755";
 
 
-class aboutSection extends Component{
-	render(){
-		return(
-			<>
-				
-				<footer className="footer">
-					<div className="footer-info bg-primary">
-						<div className="container">
-							<div className="row align-items-center">
-								<div className="col-lg-3 col-md-6 col-sm-6 mb-30">
-									<div className="footer-logo">
-										<img src={logo2} alt=""/> 
+function useFooter() {
+	return (
+		<>
+			<footer className="footer">
+				<div className="footer-info bg-primary">
+					<div className="container">
+						<div className="row align-items-center">
+							<div className="col-lg-3 col-md-6 col-sm-6 mb-30">
+								<div className="footer-logo">
+									<img src={logo2} alt="" />
+								</div>
+							</div>
+							<div className="col-lg-3 col-md-6 col-sm-6 mb-30">
+								<div className="feature-container left footer-info-bx">
+									<div className="icon-lg">
+										<span className="icon-cell"><img src={icon3} alt="" /></span>
+									</div>
+									<div className="icon-content">
+										<p>Industrial Park, 218 Hollies Way, High St, Potters Bar EN6 5BY</p>
 									</div>
 								</div>
-								<div className="col-lg-3 col-md-6 col-sm-6 mb-30">
-									<div className="feature-container left footer-info-bx">
-										<div className="icon-lg">
-											<span className="icon-cell"><img src={icon3} alt=""/></span> 
-										</div>
-										<div className="icon-content">
-											<p>Industrial Park, 218 Hollies Way, High St, Potters Bar EN6 5BY</p>
-										</div>
+							</div>
+							<div className="col-lg-3 col-md-6 col-sm-6 mb-30">
+								<div className="feature-container left footer-info-bx">
+									<div className="icon-lg">
+										<span className="icon-cell"><img src={icon1} alt="" /></span>
+									</div>
+									<div className="icon-content">
+										<p>{telareacode} {telnumber1}<br />{telareacode} {telnumber2}</p>
 									</div>
 								</div>
-								<div className="col-lg-3 col-md-6 col-sm-6 mb-30">
-									<div className="feature-container left footer-info-bx">
-										<div className="icon-lg">
-											<span className="icon-cell"><img src={icon1} alt=""/></span> 
-										</div>
-										<div className="icon-content">
-											<p>{telareacode} {telnumber1}<br/>{telareacode} {telnumber2}</p>
-										</div>
+							</div>
+							<div className="col-lg-3 col-md-6 col-sm-6 mb-30">
+								<div className="feature-container left footer-info-bx">
+									<div className="icon-lg">
+										<span className="icon-cell"><img src={icon2} alt="" /></span>
 									</div>
-								</div>
-								<div className="col-lg-3 col-md-6 col-sm-6 mb-30">
-									<div className="feature-container left footer-info-bx">
-										<div className="icon-lg">
-											<span className="icon-cell"><img src={icon2} alt=""/></span> 
-										</div>
-										<div className="icon-content">
-											<Link to="/contact-us" className="btn btn-warning">Contact Us</Link>
-										</div>
+									<div className="icon-content">
+										<Link to="/contact-us" className="btn btn-warning">Contact Us</Link>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-					<div className="footer-top bt0">
-						<div className="container">
-							<div className="row">
-								<div className="col-xl-4 col-md-6">
-									<div className="widget footer_widget">
-										<h5 className="footer-title">Company</h5>
-										<p className="mb-20">Need a special repair service? we are happy to fulfil every request in order to exceed your expectations</p>
-										<div className="ft-content">
-											<i className="fa fa-phone"></i>
-											<span>Talk To Our Team</span>
-											<h4>{telareacode} {telnumber1}</h4>
-										</div>
+				</div>
+				<div className="footer-top bt0">
+					<div className="container">
+						<div className="row">
+							<div className="col-xl-4 col-md-6">
+								<div className="widget footer_widget">
+									<h5 className="footer-title">Company</h5>
+									<p className="mb-20">Need a special repair service? we are happy to fulfil every request in order to exceed your expectations</p>
+									<div className="ft-content">
+										<i className="fa fa-phone"></i>
+										<span>Talk To Our Team</span>
+										<h4>{telareacode} {telnumber1}</h4>
 									</div>
 								</div>
-								<div className="col-xl-4 col-md-6">
-									<div className="widget widget-link-2">
-										<h5 className="footer-title">Our Solutions</h5>
-										<ul>
-											<li><Link to="/about-us">About Us</Link></li>
-											<li><Link to="#">Engine Services</Link></li>
-											<li><Link to="/booking">Booking</Link></li>
-											<li><Link to="#">Oil And Filters</Link></li>
-											<li><Link to="/services">Our Services</Link></li>
-											<li><Link to="#">Belts And Hoses</Link></li>
-											<li><Link to="/servicing">Servicing</Link></li>
-											<li><Link to="#">Brake Repair</Link></li>
-											<li><Link to="/team">Out Team</Link></li>
-											<li><Link to="#">Air Conditioning</Link></li>
-											<li><Link to="/contact-us">Contact Us</Link></li>
-											<li><Link to="#">Tire And Wheel</Link></li>
-										</ul>							
-									</div>
+							</div>
+							<div className="col-xl-4 col-md-6">
+								<div className="widget widget-link-2">
+									<h5 className="footer-title">Our Solutions</h5>
+									<ul>
+										<li><Link to="/about-us">About Us</Link></li>
+										<li><Link to="#">Engine Services</Link></li>
+										<li><Link to="/booking">Booking</Link></li>
+										<li><Link to="#">Oil And Filters</Link></li>
+										<li><Link to="/services">Our Services</Link></li>
+										<li><Link to="#">Belts And Hoses</Link></li>
+										<li><Link to="/servicing">Servicing</Link></li>
+										<li><Link to="#">Brake Repair</Link></li>
+										<li><Link to="/team">Out Team</Link></li>
+										<li><Link to="#">Air Conditioning</Link></li>
+										<li><Link to="/contact-us">Contact Us</Link></li>
+										<li><Link to="#">Tire And Wheel</Link></li>
+									</ul>
 								</div>
-								<div className="col-xl-4 col-md-6">
-									<div className="widget widget_info">
-										<h5 className="footer-title">Subscribe Now</h5>
-										<p className="mb-20">Weekly Breaking News Analysis And Cutting Edge Advices On Job Searching.</p>
-										<form className="subscribe-form subscription-form mb-30">
-											<div className="ajax-message"></div>
-											<div className="input-group">
-												<input name="email" required="required" className="form-control" placeholder="Email Address" type="email"/>
-												<div className="input-group-append">
-													<button name="submit" value="Submit" type="submit" className="btn btn-block btn-primary radius-sm">Send</button>	
-												</div>
+							</div>
+							<div className="col-xl-4 col-md-6">
+								<div className="widget widget_info">
+									<h5 className="footer-title">Subscribe Now</h5>
+									<p className="mb-20">Weekly Breaking News Analysis And Cutting Edge Advices On Job Searching.</p>
+									<form className="subscribe-form subscription-form mb-30">
+										<div className="ajax-message"></div>
+										<div className="input-group">
+											<input name="email" required="required" className="form-control" placeholder="Email Address" type="email" />
+											<div className="input-group-append">
+												<button name="submit" value="Submit" type="submit" className="btn btn-block btn-primary radius-sm">Send</button>
 											</div>
-										</form>
-										<ul className="list-inline ft-social-bx">
-											<li><a target="_blank" rel="noreferrer" href="https://www.facebook.com/JTTAutotech/" className="btn button-sm"><i className="fa fa-facebook"></i></a></li>
-											{/*<li><a target="_blank" rel="noreferrer" href="https://twitter.com/" className="btn button-sm"><i className="fa fa-twitter"></i></a></li>
+										</div>
+									</form>
+									<ul className="list-inline ft-social-bx">
+										<li><a target="_blank" rel="noreferrer" href="https://www.facebook.com/JTTAutotech/" className="btn button-sm"><i className="fa fa-facebook"></i></a></li>
+										{/*<li><a target="_blank" rel="noreferrer" href="https://twitter.com/" className="btn button-sm"><i className="fa fa-twitter"></i></a></li>
 											<li><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/" className="btn button-sm"><i className="fa fa-linkedin"></i></a></li>
 											<li><a target="_blank" rel="noreferrer" href="https://www.instagram.com/" className="btn button-sm"><i className="fa fa-instagram"></i></a></li>*/}
-										</ul>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="footer-bottom">
-						<div className="container">
-							<div className="row">
-								<div className="col-lg-6 col-md-6 col-sm-12 text-center text-md-start">
-									<p className="mb-0">© Copyright JTT Autotech {(new Date().getFullYear())}. All right reserved.</p>
-								</div>
-								<div className="col-lg-6 col-md-6 col-sm-12 text-center text-md-end">
-									<ul className="widget-link">
-										<li><Link to="/index">Home</Link></li>
-										<li><Link to="/about-us">About</Link></li>
-										<li><Link to="/terms-of-use">Terms of Use</Link></li>
-										<li><Link to="/services">Services</Link></li>
-										<li><Link to="/contact-us">Contact</Link></li>
 									</ul>
 								</div>
 							</div>
 						</div>
 					</div>
-				</footer>
-			
-			</>
-		);
-	}
+				</div>
+				<div className="footer-bottom">
+					<div className="container">
+						<div className="row">
+							<div className="col-lg-6 col-md-6 col-sm-12 text-center text-md-start">
+								<p className="mb-0">© Copyright JTT Autotech {(new Date().getFullYear())}. All right reserved.</p>
+							</div>
+							<div className="col-lg-6 col-md-6 col-sm-12 text-center text-md-end">
+								<ul className="widget-link">
+									<li><Link to="/index">Home</Link></li>
+									<li><Link to="/about-us">About</Link></li>
+									<li><Link to="/terms-of-use">Terms of Use</Link></li>
+									<li><Link to="/services">Services</Link></li>
+									<li><Link to="/contact-us">Contact</Link></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+
+		</>
+	);
 }
 
-export default aboutSection;
+export default useFooter;
--- src/markup/layout/header.jsx
+++ src/markup/layout/header.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Sticky from 'react-stickynode';
 
@@ -9,152 +9,149 @@ import logoWhite from "../../images/logo-white.png";
 const telareacode = "01707";
 const telnumber1 = "658899";
 
-class aboutSection extends Component{
+function useHeader() {
 
-	
-	componentDidMount(){
-		
+	useEffect(() => {
+
 		// Mobile Menu Open
 		var menuIcon = document.querySelector(".menuicon")
 		var menuLinks = document.querySelector(".menu-links")
 		var menuClose = document.getElementById("menuClose")
-		
-		menuIcon.addEventListener('click',function(){
+
+		menuIcon.addEventListener('click', function () {
 			menuLinks.classList.add("show")
 		})
 
-		menuClose.addEventListener('click',function(){
+		menuClose.addEventListener('click', function () {
 			menuLinks.classList.remove("show")
 		})
-		
+
 		// Mobile Submenu Open Close Function
 		var navMenu = [].slice.call(document.querySelectorAll('.menu-links > ul > li'));
 		for (var y = 0; y < navMenu.length; y++) {
 			navMenu[y].addEventListener('click', function () { menuClick(this) });
 		}
-	
+
 		function menuClick(current) {
 			const active = current.classList.contains("open")
 			navMenu.forEach(el => el.classList.remove('open'));
-			
-			if(active){
-				current.classList.remove('open') 
+
+			if (active) {
+				current.classList.remove('open')
 				console.log("active")
-			} else{
+			} else {
 				current.classList.add('open');
 				console.log("close")
 			}
 		}
-	}
-	
-	render(){
-		return(
-			<>
-				
-				<header className="header header-transparent rs-nav">
-					<Sticky enabled={true} className="sticky-header navbar-expand-lg">
-						<div className="menu-bar clearfix ">
-							<div className="container clearfix">
+	})
+
+	return (
+		<>
+
+			<header className="header header-transparent rs-nav">
+				<Sticky enabled={true} className="sticky-header navbar-expand-lg">
+					<div className="menu-bar clearfix ">
+						<div className="container clearfix">
+							<div className="menu-logo">
+								<Link to="/" className="main-logo"><img src={logoWhite} alt="" /></Link>
+								<Link to="/" className="sticky-logo"><img src={logo} alt="" /></Link>
+							</div>
+							<button className="navbar-toggler collapsed menuicon justify-content-end" type="button" data-toggle="collapse" data-target="#menuDropdown" aria-controls="menuDropdown" aria-expanded="false" aria-label="Toggle navigation">
+								<span></span>
+								<span></span>
+								<span></span>
+							</button>
+							<div className="secondary-menu">
+								<a href={"tel:" + telareacode + telnumber1}><span className="btn btn-outline-light">{telareacode + " " + telnumber1}</span></a>
+								<Link to="/booking" className="btn btn-primary ms-3">Booking</Link>
+							</div>
+							<div className="menu-links navbar-collapse collapse justify-content-center" id="menuDropdown">
 								<div className="menu-logo">
-									<Link to="/" className="main-logo"><img src={logoWhite} alt=""/></Link>
-									<Link to="/" className="sticky-logo"><img src={logo} alt=""/></Link>
+									<Link to="/"><img src={logoWhite} alt="" /></Link>
 								</div>
-								<button className="navbar-toggler collapsed menuicon justify-content-end" type="button" data-toggle="collapse" data-target="#menuDropdown" aria-controls="menuDropdown" aria-expanded="false" aria-label="Toggle navigation">
-									<span></span>
-									<span></span>
-									<span></span>
-								</button>
-								<div className="secondary-menu">
-								<a href={"tel:"+telareacode+telnumber1}><span className="btn btn-outline-light">{telareacode + " " + telnumber1}</span></a>
-									<Link to="/booking" className="btn btn-primary ms-3">Booking</Link>
-								</div>
-								<div className="menu-links navbar-collapse collapse justify-content-center" id="menuDropdown">
-									<div className="menu-logo">
-										<Link to="/"><img src={logoWhite} alt=""/></Link>
-									</div>
-									<ul className="nav navbar-nav">	
-										<li className="active">
-											<Link to="/">Home</Link>
-										</li>
-										<li>
-											<Link to="/mot">MOT</Link>
-										</li>
-										<li>
-											<Link to="/servicing">Servicing</Link>
-										</li>
-										<li className="has-mega-menu "><Link to="#">Pages<i className="fa fa-chevron-down"></i></Link>
-											<ul className="mega-menu">
-												<li>
-													<Link to="#">Pages</Link>
-													<ul>
-														<li><Link to="/about-1"><span>About Us 1</span></Link></li>
-														<li><Link to="/about-2"><span>About Us 2</span></Link></li>
-														<li><Link to="/service-1"><span>Service 1</span></Link></li>
-														<li><Link to="/service-2"><span>Service 2</span></Link></li>
-													</ul>
-												</li>
-												<li>
-													<Link to="#">Pages</Link>
-													<ul>
-														<li><Link to="/team"><span>Our Team</span></Link></li>
-														<li><Link to="/team-details"><span>Team Details</span></Link></li>
-														<li><Link to="/job-career"><span>Job & Career</span></Link></li>
-														<li><Link to="/terms-of-use"><span>Terms of Use</span></Link></li>
-													</ul>
-												</li>
-												<li>
-													<Link to="#">Pages</Link>
-													<ul>
-														<li><Link to="/booking"><span>Booking</span></Link></li>
-														<li><Link to="/servicing"><span>Servicing</span></Link></li>
-													</ul>
-												</li>
-												<li>
-													<Link to="#">Pages</Link>
-													<ul>
-														<li><Link to="/error-404"><span>Error 404</span></Link></li>
-														<li><Link to="/contact-1"><span>Contact Us 1</span></Link></li>
-														<li><Link to="/contact-us"><span>Contact Us</span></Link></li>
-													</ul>
-												</li>
-											</ul>
-										</li>
-										<li>
-											<Link to="#">Services<i className="fa fa-chevron-down"></i></Link>
-											<ul className="sub-menu">
-												<li className="add-menu-left">
-													<ul>
-														<li><Link to="/service-engine-diagnostics"><span>Engine Diagnostics</span> </Link></li>
-														<li><Link to="/service-lube-oil-and-filters"><span>Lube Oil and Filters</span></Link></li>
-														<li><Link to="/service-belts-and-hoses"><span>Belts and Hoses</span></Link></li>
-														<li><Link to="/service-air-conditioning"><span>Air Conditioning</span></Link></li>
-														<li><Link to="/service-brake-repair"><span>Brake Repair</span></Link></li>
-														<li><Link to="/service-tire-and-wheel-services"><span>Tire and Wheel Services</span></Link></li>
-													</ul>
-												</li>
-											</ul>
-										</li>
-										<li><Link to="/contact-us">Contact</Link>
-										</li>
-									</ul>
-									<ul className="social-media">
-										<li><a target="_blank" href="https://www.facebook.com/" rel="noreferrer" className="btn btn-primary"><i className="fa fa-facebook"></i></a></li>
-										<li><a target="_blank" href="https://www.google.com/" rel="noreferrer" className="btn btn-primary"><i className="fa fa-google-plus"></i></a></li>
-										<li><a target="_blank" href="https://www.linkedin.com/" rel="noreferrer" className="btn btn-primary"><i className="fa fa-linkedin"></i></a></li>
-										<li><a target="_blank" href="https://twitter.com/" rel="noreferrer" className="btn btn-primary"><i className="fa fa-twitter"></i></a></li>
-									</ul>
-									<div className="menu-close" id="menuClose">
-										<i className="ti-close"></i>
-									</div>
+								<ul className="nav navbar-nav">
+									<li className="active">
+										<Link to="/">Home</Link>
+									</li>
+									<li>
+										<Link to="/mot">MOT</Link>
+									</li>
+									<li>
+										<Link to="/servicing">Servicing</Link>
+									</li>
+									<li className="has-mega-menu "><Link to="#">Pages<i className="fa fa-chevron-down"></i></Link>
+										<ul className="mega-menu">
+											<li>
+												<Link to="#">Pages</Link>
+												<ul>
+													<li><Link to="/about-1"><span>About Us 1</span></Link></li>
+													<li><Link to="/about-2"><span>About Us 2</span></Link></li>
+													<li><Link to="/service-1"><span>Service 1</span></Link></li>
+													<li><Link to="/service-2"><span>Service 2</span></Link></li>
+												</ul>
+											</li>
+											<li>
+												<Link to="#">Pages</Link>
+												<ul>
+													<li><Link to="/team"><span>Our Team</span></Link></li>
+													<li><Link to="/team-details"><span>Team Details</span></Link></li>
+													<li><Link to="/job-career"><span>Job & Career</span></Link></li>
+													<li><Link to="/terms-of-use"><span>Terms of Use</span></Link></li>
+												</ul>
+											</li>
+											<li>
+												<Link to="#">Pages</Link>
+												<ul>
+													<li><Link to="/booking"><span>Booking</span></Link></li>
+													<li><Link to="/servicing"><span>Servicing</span></Link></li>
+												</ul>
+											</li>
+											<li>
+												<Link to="#">Pages</Link>
+												<ul>
+													<li><Link to="/error-404"><span>Error 404</span></Link></li>
+													<li><Link to="/contact-1"><span>Contact Us 1</span></Link></li>
+													<li><Link to="/contact-us"><span>Contact Us</span></Link></li>
+												</ul>
+											</li>
+										</ul>
+									</li>
+									<li>
+										<Link to="#">Services<i className="fa fa-chevron-down"></i></Link>
+										<ul className="sub-menu">
+											<li className="add-menu-left">
+												<ul>
+													<li><Link to="/service-engine-diagnostics"><span>Engine Diagnostics</span> </Link></li>
+													<li><Link to="/service-lube-oil-and-filters"><span>Lube Oil and Filters</span></Link></li>
+													<li><Link to="/service-belts-and-hoses"><span>Belts and Hoses</span></Link></li>
+													<li><Link to="/service-air-conditioning"><span>Air Conditioning</span></Link></li>
+													<li><Link to="/service-brake-repair"><span>Brake Repair</span></Link></li>
+													<li><Link to="/service-tire-and-wheel-services"><span>Tire and Wheel Services</span></Link></li>
+												</ul>
+											</li>
+										</ul>
+									</li>
+									<li><Link to="/contact-us">Contact</Link>
+									</li>
+								</ul>
+								<ul className="social-media">
+									<li><a target="_blank" href="https://www.facebook.com/" rel="noreferrer" className="btn btn-primary"><i className="fa fa-facebook"></i></a></li>
+									<li><a target="_blank" href="https://www.google.com/" rel="noreferrer" className="btn btn-primary"><i className="fa fa-google-plus"></i></a></li>
+									<li><a target="_blank" href="https://www.linkedin.com/" rel="noreferrer" className="btn btn-primary"><i className="fa fa-linkedin"></i></a></li>
+									<li><a target="_blank" href="https://twitter.com/" rel="noreferrer" className="btn btn-primary"><i className="fa fa-twitter"></i></a></li>
+								</ul>
+								<div className="menu-close" id="menuClose">
+									<i className="ti-close"></i>
 								</div>
 							</div>
 						</div>
-					</Sticky>
-				</header>
-			
-			</>
-		);
-	}
+					</div>
+				</Sticky>
+			</header>
+
+		</>
+	);
 }
 
-export default aboutSection;
+export default useHeader;
